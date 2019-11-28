44f4dc7b0893a700cb39a5410ff5385870a59237
Dreamtowards <30804116+Dreamtowards@users.noreply.github.com>
2019-11-28 13:30:20 +0800

    Add files via upload

--- src/outskirts/client/material/Model.java
+++ src/outskirts/client/material/Model.java
@@ -4,12 +4,20 @@ import static org.lwjgl.opengl.ARBVertexArrayObject.glDeleteVertexArrays;
 import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.opengl.GL15.glDeleteBuffers;
 
+/**
+ * uses a public instance field, though "faster" (both exec speed and write/call speed), "convenience"
+ * but its given people a Casual feel.. like a tmp design, and feels unsafe cause its some way is a public data.
+ *
+ * Method given people more Trust && StrongPower than public Field in subconscious
+ * those getter not "get" prefix cause the prefix have some turn down the mainly feel.
+ */
 public class Model {
 
     private int vaoID;
-    /** If not EBO, that eboID will be -1 */
+
+    /** If not EBO, (use GL_ARRAY_BUFFER) that eboID will be -1 */
     private int eboID;
-    /** vboIDs */
+
     private int[] vboID;
 
     private int vertexCount;
@@ -21,9 +29,6 @@ public class Model {
         this.vertexCount = vertexCount;
     }
 
-    /**
-     * Method given people more Trust && StrongPower than Field in subconscious
-     */
     public int vaoID() {
         return vaoID;
     }
